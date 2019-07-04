b3024a7113ab867f7dab11a4974b3082195299e7
Ahd <dafuq.ahd@gmail.com>
2019-07-04 21:02:40 +0300

    styling stack section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -18,51 +18,60 @@ const Skills = () => (
             src={require("../assets/imgs/icons8-javascript.svg")}
             alt="skill-logo"
           />
+          <p>JavaScript</p>
         </div>
         <div>
           <img
             src={require("../assets/imgs/icons8-nodejs (1).svg")}
             alt="skill-logo"
           />
+          <p>NodeJS</p>
         </div>
         <div>
           <img
             src={require("../assets/imgs/icons8-python.svg")}
             alt="skill-logo"
           />
+          <p>Python</p>
         </div>
         <div>
           <img
             src={require("../assets/imgs/icons8-react.svg")}
             alt="skill-logo"
           />
+          <p>ReactJS</p>
         </div>
         <div>
           <img
             src={require("../assets/imgs/icons8-react-native-filled.svg")}
             alt="skill-logo"
           />
+          <p>React Native</p>
         </div>
         <div>
           <img src={require("../assets/imgs/redux.svg")} alt="skill-logo" />
+          <p>Redux</p>
         </div>
         <div>
           <img
             src={require("../assets/imgs/icons8-mongodb.svg")}
             alt="skill-logo"
           />
+          <p>MongoDB</p>
         </div>
         <div>
           <img
             src={require("../assets/imgs/icons8-css3-filled.svg")}
             alt="skill-logo"
           />
+          <p>CSS & SASS</p>
         </div>
         <div>
           <img
             src={require("../assets/imgs/icons8-html-5.svg")}
             alt="skill-logo"
           />
+          <p>HTML5</p>
         </div>
       </div>
     </div>
